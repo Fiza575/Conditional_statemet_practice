d0d67d95d537f67f19bdd6a7a6297a529b19c922
Fiza Arzoo Khalid <shaizakanwal025@gmail.com>
2025-05-01 12:09:21 +0500

    Add files via upload

---
+++ Conditional_statements.py
@@ -0,0 +1,14 @@
+#Music App
+mood =input("Tell us about your current mood?:")
+if mood =="happy":
+    print("Play upbeat music")
+elif mood =="sad":
+    print("play Slow tempo and relaxed music")
+elif mood =="angry":
+    print("Play fast tempo,intense lyrics music")
+elif mood =="relaxed":
+    print("play calm and peaceful music")
+elif mood ==" energetic":
+    print("Play excited and full of life music")  
+else:
+    print("play mix music playlist")
